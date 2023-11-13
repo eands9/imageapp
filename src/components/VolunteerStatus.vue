c9d92dee6b909fe6f913b03429b8a47e97b0ec41
eands9@yahoo.com <eands9@yahoo.com>
2023-11-13 16:22:05 -0600

    testing circular progress

--- src/components/VolunteerStatus.vue
+++ src/components/VolunteerStatus.vue
@@ -43,7 +43,7 @@ function submitForm() {
 
 }
 async function getData(){
-    // const id = state.gender;
+    const id = state.gender;
 
     // const gql = `
     // query getById($id: ID!) {
@@ -61,7 +61,7 @@ async function getData(){
     //   },
     // };
 
-    const email = state.email;
+    // const email = state.email;
 
     const gql = `
     query getById($id: ID!) {
@@ -74,7 +74,7 @@ async function getData(){
     const query = {
       query: gql,
       variables: {
-        email: email,
+        id: id,
       },
     };
 
