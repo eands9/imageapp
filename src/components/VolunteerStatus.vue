1217f5cf70b67562a471f834a6d1d0278ed67088
eands9@yahoo.com <eands9@yahoo.com>
2023-11-13 16:16:48 -0600

    testing circular progress

--- src/components/VolunteerStatus.vue
+++ src/components/VolunteerStatus.vue
@@ -65,7 +65,7 @@ async function getData(){
 
     const gql = `
     query getById($id: ID!) {
-      imageapp_by_pk(email: $email) {
+      imageapp_by_pk(id: $id) {
         id,
         email,
         gender
